2c736be37b9b3e92411f5c6c138fff96428727c5
Jason Zhang <arktests87@gmail.com>
2026-05-02 00:14:33 +0000

    problem sql

--- 311_complaints_models/problem_type_dimension.sql
+++ 311_complaints_models/problem_type_dimension.sql
@@ -1,14 +1,15 @@
- {{
-      config(
-          materialized='table'
-      )
-}}
 WITH problem_type_columns AS
 (
-    SELECT DISTINCT problem_type, problem_detail
+    SELECT DISTINCT 
+    problem_type, 
+    problem_detail
+  FROM {{ ref('raw_311_complaints') }}
+  WHERE problem_type IS NOT NULL
 )
 SELECT 
-    row_number() OVER() AS agency_dim_id,
-    problem_type, problem_detail, current_timestamp() as loaded_at
+    ROW_NUMBER() OVER() AS problem_type_dimension_id,
+    problem_type, 
+    problem_detail, 
+    CURRENT_TIMESTAMP() as loaded_at
 FROM problem_type_columns
 ORDER BY problem_type, problem_detail
